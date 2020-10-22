8b691472595281ac373bde00190227b35231dc03
Bárbara Santana <55034150+babqs@users.noreply.github.com>
2020-10-22 14:45:17 -0300

    Add files via upload

--- BFS/src/bfs/BreadthFirstSearch.java
+++ BFS/src/bfs/BreadthFirstSearch.java
@@ -56,20 +56,36 @@ public class BreadthFirstSearch {
 
     public static void main(String[] args) {
 
-        BreadthFirstSearch g = new BreadthFirstSearch(6);
+        BreadthFirstSearch g = new BreadthFirstSearch(21);
 
-        g.addEdge(0, 1);
-        g.addEdge(0, 2);
-        g.addEdge(1, 2);
-        g.addEdge(2, 0);
-        g.addEdge(2, 3);
-        g.addEdge(3, 4);
-        g.addEdge(4, 5);
-        g.addEdge(5, 5);
+    g.addEdge(0, 3);
+    g.addEdge(0, 1);
+    g.addEdge(1, 2);
+    g.addEdge(1, 4);
+    g.addEdge(2, 3);
+    g.addEdge(3, 1);
+    g.addEdge(3, 5);
+    g.addEdge(4, 5);
+    g.addEdge(5, 1);
+    g.addEdge(6, 0);
+    g.addEdge(7, 2);
+    g.addEdge(8, 5);
+    g.addEdge(9, 6);
+    g.addEdge(10, 8);
+    g.addEdge(11, 8);
+    g.addEdge(12, 7);
+    g.addEdge(13, 5);
+    g.addEdge(14, 6);
+    g.addEdge(15, 8);
+    g.addEdge(16, 9);
+    g.addEdge(17, 4);
+    g.addEdge(18, 7);
+    g.addEdge(19, 1);
+    g.addEdge(20, 17);
 
         Scanner s = new Scanner(System.in);
 
-        System.out.print("Escolha um numero de 0-5 para o vertice inicial: ");
+        System.out.print("Escolha um numero de 0-21 para o vertice inicial: ");
         int vInicial = s.nextInt();
 
         g.BFS(vInicial);
